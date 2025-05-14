8c42ea84e707e3113ddcc4d8fc56ebecba3ce5ef
j <jforsberg.se@gmail.com>
2025-05-14 13:09:22 +0200

    Added healthcheck, showing both uptime and commit hash

--- app/routers/web.py
+++ app/routers/web.py
@@ -5,10 +5,12 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 import subprocess
 import os
+import time
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
+start_time = time.time()
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
@@ -24,11 +26,24 @@ def get_git_commit_hash() -> str:
         return commit_hash
     except subprocess.CalledProcessError:
         return "Unknown git command"
+    
+def get_uptime() -> str:
+    # Calc uptime in s, m, h
+    uptime_seconds = int(time.time() - start_time)
+    hours = uptime_seconds // 3600
+    minutes = (uptime_seconds % 3600) // 60
+    seconds = uptime_seconds % 60
+    return f"{hours}h {minutes}m {seconds}s"
 
 @router.get("/status", response_class=HTMLResponse)
 async def status(request: Request):
-    commit_hash = get_git_commit_hash()
+    # commit_hash = get_git_commit_hash()
+    health_data = {
+        "status": "Healthy",
+        "commit_hash": get_git_commit_hash(),
+        "uptime": get_uptime()
+    }
     return templates.TemplateResponse(
         "status.html",
-        { "request": request, "commit_hash": commit_hash }
+        { "request": request, **health_data }
     )
