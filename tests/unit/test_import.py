8488b794c67551909184f2ea3e1f92a0814c2ec4
ShaheeraMudasar <shaheeramudasar@gmail.com>
2025-05-22 12:51:09 +0200

    commented all

--- tests/unit/test_import.py
+++ tests/unit/test_import.py
@@ -1,3 +1,3 @@
 # Detta ser till att all filer som appen använder räknas in i coverage
-def test_import_all():
-    import app.main  # noqa: F401
+#def test_import_all():
+ #   import app.main  # noqa: F401
