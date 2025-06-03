5a1e8b498a4f2eaf04518bf047851a46c26441ab
j <jforsberg.se@gmail.com>
2025-06-03 18:48:27 +0200

    Added a quick integration test for workflow check

---
+++ tests/integration/test_delete_post.py
@@ -0,0 +1,27 @@
+import boto3
+from hamcrest import assert_that, contains_string, equal_to, is_
+
+from app.storage.ddb import DynamoClient
+from app.models import PostIn
+
+def test_delete_post_from_db():
+    client = DynamoClient()
+    posts = len(client.list_posts())
+
+    post = PostIn(
+        title = "title_test_post",
+        image_url = "testurl.com",
+        image_text = "test_text"
+    )
+
+    post_id = client.create_post(post)
+
+    posts_after_create = len(client.list_posts())
+
+    assert_that(posts_after_create, equal_to(posts + 1))
+
+    client.delete_post(post_id)
+
+    posts_after_delete = len(client.list_posts())
+
+    assert_that(posts_after_delete, equal_to(posts_after_create - 1))
