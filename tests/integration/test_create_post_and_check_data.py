4854a26a5e3b97b7134f71ae8980703b0cb3c3e2
j <jforsberg.se@gmail.com>
2025-06-04 15:11:35 +0200

    Created test for creating and getting post with id

--- tests/integration/test_create_post_and_check_data.py
+++ tests/integration/test_create_post_and_check_data.py
@@ -21,3 +21,25 @@ def test_create_post():
     # THEN we should get an post_id back
 
     assert_that(post_id, not_none())
+
+def test_create_post_and_get_post_with_id():
+    # GIVEN that we have a client and a post
+
+    client = DynamoClient()
+
+    new_post = PostIn(
+        title="This is a test post", 
+        image_url="thisisthetesturl.com", 
+        image_text="This is the image text"
+    )
+
+    post_id = client.create_post(new_post)
+
+    # WHEN we use that post_id to get a specific post
+
+    post = client.get_post(post_id)
+    print(post)
+
+    # THEN we should get the post in return
+
+    assert_that(post, not_none())
