fa2d0d1df787c132e538d9820679b9e3b6ffee3b
Shaheera Mudasar <shaheeramudasar@gmail.com>
2025-06-04 12:50:36 +0200

    Fixed linting in new file

---
+++ tests/integration/test_create_post_with_test_client.py
@@ -0,0 +1,30 @@
+from uuid import uuid4  # for unique post title
+
+from fastapi.testclient import TestClient
+from hamcrest import assert_that, equal_to
+
+from app.start import app
+from app.storage.ddb import DynamoClient
+
+
+def test_create_post_adds_post_in_db():
+    # GIVEN post details are given
+    client = TestClient(app)
+    db = DynamoClient()
+
+    unique_title = f"test-title-{uuid4()}"
+
+    form_data = {
+        "password": "admin123",
+        "title": unique_title,
+        "image_url": "https://test.com/image",
+        "image_text": "test_image",
+    }
+
+    response = client.post("admin/create", data=form_data, follow_redirects=False)
+
+    assert_that(response.status_code, equal_to(302))
+
+    posts = db.list_posts()
+    matched_post = [p for p in posts if p.title == unique_title]
+    assert_that(len(matched_post), equal_to(1))
