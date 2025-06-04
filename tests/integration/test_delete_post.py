d44eb01f1c470e8ae43c27e14089f8be7f08d219
j <jforsberg.se@gmail.com>
2025-06-04 12:34:30 +0200

    Added GIVEN,WHEN,THEN to the test

--- tests/integration/test_delete_post.py
+++ tests/integration/test_delete_post.py
@@ -5,7 +5,9 @@ from app.storage.ddb import DynamoClient
 
 
 def test_delete_post_from_db():
+    # GIVEN That we have client and a post in the database
     client = DynamoClient()
+
     posts = len(client.list_posts())
 
     post = PostIn(title="title_test_post", image_url="testurl.com", image_text="test_text")
@@ -14,10 +16,13 @@ def test_delete_post_from_db():
 
     posts_after_create = len(client.list_posts())
 
+    # A check that the post was actually created.
     assert_that(posts_after_create, equal_to(posts + 1))
 
+    # WHEN We then delete the post with the ID we get back from create post.
     client.delete_post(post_id)
 
     posts_after_delete = len(client.list_posts())
 
+    # THEN The length of the posts list should be -1
     assert_that(posts_after_delete, equal_to(posts_after_create - 1))
