d735f6484eb5f326f2d93363d792feced35b2230
j <jforsberg.se@gmail.com>
2025-06-04 15:11:03 +0200

    Added test to create post

--- tests/integration/test_create_post_and_check_data.py
+++ tests/integration/test_create_post_and_check_data.py
@@ -2,3 +2,22 @@ from hamcrest import assert_that, not_none, equal_to
 
 from app.models import PostIn, PostOut
 from app.storage.ddb import DynamoClient
+
+def test_create_post():
+    # GIVEN that we have a client
+
+    client = DynamoClient()
+
+    # WHEN we create a new post
+    
+    post = PostIn(
+        title="This is a test post", 
+        image_url="thisisthetesturl.com", 
+        image_text="This is the image text"
+    )
+
+    post_id = client.create_post(post)
+
+    # THEN we should get an post_id back
+
+    assert_that(post_id, not_none())
