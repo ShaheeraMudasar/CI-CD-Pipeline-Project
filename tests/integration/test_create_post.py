47fbbacfd3ce31596d7a7915ebb3a44f195b7c9c
Shaheera Mudasar <shaheeramudasar@gmail.com>
2025-06-04 09:49:12 +0200

    Wrote test for create post using testclient

--- tests/integration/test_create_post.py
+++ tests/integration/test_create_post.py
@@ -1,28 +1,41 @@
 from hamcrest import assert_that, equal_to
+from fastapi.testclient import TestClient
 
 from app.models import PostIn
 from app.storage.ddb import DynamoClient
+from app.start import app
 
 
 def test_create_post_adds_post_in_db():
     # GIVEN post details are given
-    post = PostIn(
-        id="test_post_id",
-        title="test_post_title",
-        image_url="https://hereismyimage.com",
-        image_text="test_image",
-    )
+    client = TestClient(app)
+    db = DynamoClient()
 
-    client = DynamoClient()
-    posts = client.list_posts()
-    size_before = len(posts)
+    form_data = {
+        "password": "admin123",
+        "title" : "unique_test_post",
+        "image_url": "https://test.com/image",
+        "image_text": "test_image" 
+     }
+    
+    response = client.post("admin/create", data=form_data, follow_redirects= False)
 
-    # WHEN attempted to create a new post
+    assert_that (response.status_code, equal_to(302))
 
-    client.create_post(post)
+    posts = db.list_posts()
+    matched_post = [p for p in posts if p.title == "unique_test_post"]
+    assert_that(len(matched_post), equal_to(1))
 
-    size_after = len(client.list_posts())
+    # client = DynamoClient()
+    # posts = client.list_posts()
+    # size_before = len(posts)
 
-    # THEN it returns the list's size is increased by one
+    # # WHEN attempted to create a new post
 
-    assert_that(size_after, equal_to(size_before + 1))
+    # client.create_post(post)
+
+    # size_after = len(client.list_posts())
+
+    # # THEN it returns the list's size is increased by one
+
+    # assert_that(size_after, equal_to(size_before + 1))
