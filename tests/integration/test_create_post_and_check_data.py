d79339576f728a90cf31ef7cadfe7701ee905d07
j <jforsberg.se@gmail.com>
2025-06-04 16:01:48 +0200

    Lint fixes

--- tests/integration/test_create_post_and_check_data.py
+++ tests/integration/test_create_post_and_check_data.py
@@ -1,7 +1,8 @@
-from hamcrest import assert_that, not_none, equal_to
+from hamcrest import assert_that, equal_to, not_none
 
-from app.models import PostIn, PostOut
+from app.models import PostIn
 from app.storage.ddb import DynamoClient
+
 
 def test_create_post():
     # GIVEN that we have a client
@@ -9,11 +10,11 @@ def test_create_post():
     client = DynamoClient()
 
     # WHEN we create a new post
-    
+
     post = PostIn(
-        title="This is a test post", 
-        image_url="thisisthetesturl.com", 
-        image_text="This is the image text"
+        title="This is a test post",
+        image_url="thisisthetesturl.com",
+        image_text="This is the image text",
     )
 
     post_id = client.create_post(post)
@@ -22,15 +23,16 @@ def test_create_post():
 
     assert_that(post_id, not_none())
 
+
 def test_create_post_and_get_post_with_id():
     # GIVEN that we have a client and a post
 
     client = DynamoClient()
 
     new_post = PostIn(
-        title="This is a test post", 
-        image_url="thisisthetesturl.com", 
-        image_text="This is the image text"
+        title="This is a test post",
+        image_url="thisisthetesturl.com",
+        image_text="This is the image text",
     )
 
     post_id = client.create_post(new_post)
@@ -43,16 +45,17 @@ def test_create_post_and_get_post_with_id():
 
     assert_that(post, not_none())
 
+
 def test_create_post_and_check_data_against_get_post_with_id():
     # GIVEN that we have a client, post and data from the post
-    
+
     client = DynamoClient()
 
     # This is our data
     new_post = PostIn(
         title="This is a specific title",
         image_url="This is a specific url",
-        image_text="This is a specific text"
+        image_text="This is a specific text",
     )
 
     # This is our specific id for the post.
