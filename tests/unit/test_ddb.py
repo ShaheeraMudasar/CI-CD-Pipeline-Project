bd7bb979f10d93d96554d88e5f614701e15c9edc
Shaheera Mudasar <shaheeramudasar@gmail.com>
2025-08-19 12:41:51 +0200

    fix linting errors

--- tests/unit/test_ddb.py
+++ tests/unit/test_ddb.py
@@ -1,7 +1,10 @@
 from unittest.mock import MagicMock
+
 from hamcrest import assert_that, equal_to
+
 from app.models import PostListItem
 from app.storage.ddb import DynamoClient
+
 
 def test_list_posts_returns_sorted():
     # Create instance of the class
@@ -20,8 +23,5 @@ def test_list_posts_returns_sorted():
     result = client.list_posts()
 
     # Check the result
-    expected = [
-        PostListItem(id="1", title="First"),
-        PostListItem(id="2", title="Second")
-    ]
+    expected = [PostListItem(id="1", title="First"), PostListItem(id="2", title="Second")]
     assert_that(result, equal_to(expected))
